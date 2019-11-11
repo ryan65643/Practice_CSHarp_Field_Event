7094a51ef11afad1370268d3ff4dc44103e386a7
ryan65643 <57181200+ryan65643@users.noreply.github.com>
2019-11-11 21:37:43 +0800

    HW2

---
+++ UnityProject/Assets/Script/HW.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HW : MonoBehaviour
+{
+[Header("速度") , Range(1f,1000f) , Tooltip("速度快慢") ]
+ public float speed = 1f;
+ [Header("跳躍高度") , Range(0,10000) , Tooltip("跳多高") ]
+ public float Jump= 100;
+ [Header("對話內容") , Tooltip("聊天內容") ]
+ public string Talk =("咕咕咕~~");
+ [Header("是否取得雞蛋") , Tooltip("判斷是否取得雞蛋") ]
+ public bool Egg;
+
+
+ private void Start()
+    {
+        print("遊戲開始");
+    }
+
+
+    private void Update()
+    {
+        print("咕咕咕~~");
+    }
+}
